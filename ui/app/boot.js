80686016db9c9883a1adbf3fbc5dfc419383bd30
tvvlab <ch882010@gmail.com>
2016-06-04 23:07:34 +0200

    Client side: code editor.

--- ui/app/boot.js
+++ ui/app/boot.js
@@ -6,5 +6,9 @@
   app.controller('AppController', function($scope)
   {
     // TODO
+
+    var editor = ace.edit("editor")
+    editor.setTheme("ace/theme/monokai")
+    editor.getSession().setMode("ace/mode/javascript")
   })
 })()
