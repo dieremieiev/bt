f0f7a3e8ad305b927ce7814948dd64c62ef53638
tvvlab <ch882010@gmail.com>
2016-06-07 22:52:19 +0200

    Client side: layout

--- ui/app/boot.js
+++ ui/app/boot.js
@@ -1,12 +1,17 @@
 (function() {
-  "use strict";
+  'use strict';
 
   var app = angular.module('app', ['ngMaterial'])
 
   app.controller('AppController', function($scope)
   {
-    var editor = ace.edit("editor")
-    editor.setTheme("ace/theme/monokai")
-    editor.getSession().setMode("ace/mode/javascript")
+    var editor = ace.edit('editor')
+
+    editor.$blockScrolling = Infinity
+    editor.setTheme('ace/theme/monokai')
+    editor.getSession().setMode('ace/mode/javascript')
+
+    editor.setValue('function test() { return 1 + 1 }')
+    editor.gotoLine(0)
   })
 })()
