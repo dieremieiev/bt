94be966656428ba729567618b298d564aaf4a9aa
tvvlab <ch882010@gmail.com>
2016-10-14 23:05:42 +0200

    Added Spring Boot framework.

---
+++ api/src/main/java/net/hrobotics/bt/ApiApplication.java
@@ -0,0 +1,24 @@
+package net.hrobotics.bt;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+public class ApiApplication {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
